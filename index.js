ad6cdfd7589b5fcacb859d81c36f6024f5894a36
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-07-16 03:18:36 -0600

    got root node to display correctly

--- index.js
+++ index.js
@@ -23,18 +23,20 @@ function loadBinaryOrSearchTree(usesrchtree)
 }
 
 class Bintreend {
-    constructor(myid, myptnd, myleftkdnd, myrightkdnd)
+    constructor(myid, mydatstr, myptnd=null, myleftkdnd=null, myrightkdnd=null)
     {
         this.id = myid;
+        this.data = mydatstr;
         this.ptnd = myptnd;
         this.leftkd = myleftkdnd;
         this.rightkd = myrightkdnd;
     }
+    isRootNode() { return ((this.ptnd == null) ? true : false); }
 }
 
 function getDOMElements(ndobj)
 {
-    //
+    console.error("NOT DONE YET 7-15-2023 2:53 AM!");
 }
 
 function buildUserBinaryTree()
@@ -43,6 +45,11 @@ function buildUserBinaryTree()
     //have a textarea for data
     //have a spot for the left kid and a spot for the right kid
     //need to have an add left, add right, edit data or remove option
+    let mytable = document.createElement("table");
+    let mytrw = document.createElement("tr");
+    let mytcol = document.createElement("td");
+    let myotrw = document.createElement("tr");
+    let myotcol = document.createElement("td");
     let mydiv = document.createElement("div");
     let mycntelem = document.createElement("textarea");
     mycntelem.style.width = "248px";
@@ -50,21 +57,83 @@ function buildUserBinaryTree()
     mycntelem.style.resize = "none";
     mycntelem.style.marginLeft = "auto";
     mycntelem.style.marginRight = "auto";
-    //mydiv.id = "rtnd";
-    mydiv.style.marginLeft = "40%";
+    mydiv.id = "rtnd";
+    mytable.style.display = "inline";
+    //mydiv.style.marginLeft = "40%";
     let myleftbtn = document.createElement("button");
     let myrightbtn = document.createElement("button");
     let mydelbtn = document.createElement("button");
     let mynl = document.createElement("br");
+    //let myobr = document.createElement("br");
     myleftbtn.textContent = "add left kid";
     myrightbtn.textContent = "add right kid";
     mydelbtn.textContent = "delete node";
+    let myleftimg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    let myleftline = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    let myrightline = document.createElementNS("http://www.w3.org/2000/svg", "line");
     mydiv.appendChild(mycntelem);
     mydiv.appendChild(mynl);
-    mydiv.appendChild(myleftbtn);
-    mydiv.appendChild(myrightbtn);
-    mydiv.appendChild(mydelbtn);
-    document.getElementById("tree").appendChild(mydiv);
+    myotcol.appendChild(mydelbtn);
+    myotrw.appendChild(myotcol);
+    let mylkddiv = document.createElement("span");
+    let myrkddiv = document.createElement("span");
+    mylkddiv.appendChild(myleftbtn);
+    myrkddiv.appendChild(myrightbtn);
+    mytcol.appendChild(mydiv);
+    mytrw.appendChild(mytcol);
+    mytable.appendChild(mytrw);
+    mytable.appendChild(myotrw);
+    document.getElementById("tree").appendChild(mylkddiv);
+    document.getElementById("tree").appendChild(mytable);
+    document.getElementById("tree").appendChild(myrkddiv);
+    let mydivondoc = document.getElementById("rtnd");
+    //let myelemcoords = mydivondoc.getBoundingClientRect();
+    //console.log("myelemcoords.x = " + myelemcoords.x);
+    //console.log("myelemcoords.y = " + myelemcoords.y);
+    //console.log("myelemcoords.top = " + myelemcoords.top);
+    //console.log("myelemcoords.left = " + myelemcoords.left);
+    //console.log("myelemcoords.right = " + myelemcoords.right);
+    //console.log("myelemcoords.bottom = " + myelemcoords.bottom);
+    //console.log("topoffset = " + document.getElementById("rtnd").offsetTop);
+    //console.log("leftoffset = " + document.getElementById("rtnd").offsetLeft);
+    //console.log("heightoffset = " + document.getElementById("rtnd").offsetHeight);//actual of element
+    //console.log("widthoffset = " + document.getElementById("rtnd").offsetWidth);//actual of element
+    let mywdthstr = "" + mydivondoc.getElementsByTagName("textarea")[0].offsetWidth;
+    let mywdthnumstr = "" + mywdthstr;//.substring(0, mywdthstr.length - 2);
+    console.log("mywdthnumstr = " + mywdthnumstr);
+    myleftimg.style.width = "" + mywdthstr;
+    myleftimg.style.height = "" + Number(mywdthnumstr) / 2 + "px";
+    //0,0 is the top left corner to the svg and it is relative to its container
+    //in this case where mydiv starts after the new line
+    myrightline.setAttribute("x1", "" + Number(mywdthnumstr) / 2);
+    myrightline.setAttribute("y1", "0");
+    myrightline.setAttribute("x2", mywdthnumstr);
+    myrightline.setAttribute("y2", "" + Number(mywdthnumstr) / 2);
+    myleftline.setAttribute("x1", "" + Number(mywdthnumstr) / 2);
+    myleftline.setAttribute("y1", "0");
+    myleftline.setAttribute("x2", "0");
+    myleftline.setAttribute("y2", "" + Number(mywdthnumstr) / 2);
+    myrightline.style.stroke = "black";
+    myrightline.style.strokeWidth = "2";
+    myleftline.style.stroke = "black";
+    myleftline.style.strokeWidth = "2";
+    myleftimg.textContent = "your browser does not support svg images";
+    myleftimg.appendChild(myleftline);
+    myleftimg.appendChild(myrightline);
+    //mydivondoc.appendChild(myleftbtn);
+    mydivondoc.appendChild(myleftimg);
+    //mydivondoc.appendChild(myrightbtn);
+    //mydivondoc.appendChild(myobr);
+    //mydivondoc.appendChild(mydelbtn);
+    //console.log("myleftline.x1 = " + myleftline.x1);
+    //console.log("myleftline.y1 = " + myleftline.y1);
+    //console.log("myleftline.x2 = " + myleftline.x2);
+    //console.log("myleftline.y2 = " + myleftline.y2);
+    //console.log("firstbtnnewoffsetleft = " + mydivondoc.getElementsByTagName("button")[0].offsetLeft);
+    //console.log("topoffset = " + mydivondoc.offsetTop);
+    //console.log("leftoffset = " + mydivondoc.offsetLeft);
+    //console.log("heightoffset = " + mydivondoc.offsetHeight);//actual of element
+    //console.log("widthoffset = " + mydivondoc.offsetWidth);//actual of element
     console.error("NOT DONE YET 7-15-2023 2:53 AM!");
     debugger;
 }
